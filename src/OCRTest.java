c115672fbd11db243ad7865c343ced7b184da0ff
Yang Zhang <yangzhang333@gmail.com>
2019-03-23 01:56:36 -0400

    Added python script execution and output reading capability.

--- src/OCRTest.java
+++ src/OCRTest.java
@@ -1,9 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class OCRTest {
 
-	public static void main(String[] args) {
-		System.out.println("this is a great program");
+	public static void main(String[] args) throws IOException {
+//		System.out.println("this is a great program");
+		
+		ProcessBuilder pb = new ProcessBuilder("python","/Users/yangzhang/git/ImageTextSearch/src/testpy.py");
+		Process p = pb.start();
+		
+		//reads out the Python output
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
+		String line = "";     
+		while ((line = bfr.readLine()) != null){
+		    System.out.println("Python Output: " + line);
+		}
 	}
-
 }
---
+++ src/testpy.py
@@ -0,0 +1,11 @@
+# from mpl_toolkits.mplot3d import Axes3D 
+# from sklearn.preprocessing import StandardScaler
+# import matplotlib.pyplot as plt # plotting
+# import numpy as np # linear algebra
+# import os # accessing directory structure
+# import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+def word():
+    print "this works"
+
+word()
