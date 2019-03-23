65dedeb68cbec253bfab28f450f7972cd395a754
Jesse Galloway <jsg220@lehigh.edu>
2019-03-23 18:52:31 -0400

    deleted old db files andupdated text parser

--- src/SearchEngine/TextParser.java
+++ src/SearchEngine/TextParser.java
@@ -3,8 +3,32 @@ package SearchEngine;
 public class TextParser {
 	
 	//parse through the text file and take the strings to input as documents in the engine
-	public static String[][] ContentParser(String directoryToFiles) {
+	public static String[] ContentParser(String directoryToFiles) {
 
-		return null;	
+		String arr[][] = new String[4000][2]; 
+        int i = 0;
+        File file;
+        while (i < 3225) {
+	        file = new File("C:\\Users\\jsnga\\workspace\\ImageTextSearch\\database\\database\\"+ i + ".txt");
+        	System.out.println(file.toString());
+	        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+		            String line = br.readLine();
+		            //System.out.println(line);
+		            arr[i][0] = line;
+		            line = br.readLine();
+		            arr[i][1] = line;
+		            //System.out.println(line);
+	        } catch (Exception e) {
+	        	System.out.println("maybe there is an error with this file");
+	        }
+	        i++;
+        }
+        //i = 0;
+        // while(i < 3225) {
+        // 	System.out.println(arr[i][0] + "\t" + arr[i][1]);
+        // 	i ++;
+        // }
+
+		return arr;	
 	}
 }
