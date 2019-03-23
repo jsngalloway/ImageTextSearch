bc3f2054b736bd1ea4aa2b4219b8f5e88212ca53
Yang Zhang <yzhang51@email.wm.edu>
2019-03-23 14:18:44 -0400

    removed previous csv and python code, uploaded image link file

--- src/testpy.py
+++ src/testpy.py
@@ -1,16 +1,6 @@
-#these 3 below will cause the Java script to not recognize the rest of
-#of the python script, resulting in no output.
-
-# from mpl_toolkits.mplot3d import Axes3D
-# from sklearn.preprocessing import StandardScaler
-# import pandas as pd
-
-
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 
-def word():
-    print ("this works")
+def work():
+    print('this works')
 
-word()
+work()
